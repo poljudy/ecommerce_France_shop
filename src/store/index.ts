aeaed89bf948460fb83915868d063eab2f851e91
poljudy <franklinjin229@gmail.com>
2022-09-19 13:44:05 +0200

    store setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,33 @@
-import { createStore } from 'vuex'
-
-export default createStore({
+import { createStore } from "vuex";
+import axios from "axios";
+const store = createStore({
   state: {
+    products: [],
   },
   getters: {
+    getProducts: (state) => state.products,
   },
   mutations: {
+    SET_PRODUCTS(state, products) {
+      state.products = products;
+    },
   },
   actions: {
+    async fetchProducts({ commit }) {
+      try {
+        const data = await axios.get(
+          "https://otakod.es/hetic/ecommerce-api/products"
+        );
+        console.log(data);
+
+        commit("SET_PRODUCTS", data.data.products);
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
+
+export default store;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="home">list of products</div>
+  <div v-for="user in users" :key="user">{{ user }}</div>
 </template>
 
-<script lang="ts">
+<script>
+import { computed } from "vue";
 import { defineComponent } from "vue";
+//import the global store object from Vuex
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "HomeView",
   components: {},
+  computed: {
+    users() {
+      // return store.state.products;
+      return this.$store.getters.getProducts;
+    },
+  },
+  created() {
+    console.log(this.$store.dispatch("fetchProducts"));
+  },
 });
 </script>
