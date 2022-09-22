1b2e4626671e1e7cf31272919ca56e890171f4ab
poljudy <franklinjin229@gmail.com>
2022-09-22 09:10:22 +0200

    gallery items promo and out of stock

--- src/components/ProductsItem.vue
+++ src/components/ProductsItem.vue
@@ -1,13 +1,27 @@
 <template>
   <router-link
     :to="{ name: 'product', params: { id: product.id } }"
-    class="rounded w-56"
+    class="rounded w-56 relative"
   >
     <img
       :src="product.images.photos[0]"
       alt="`Image produit"
       class="h-72 object-cover rounded-lg"
     />
+    <div class="absolute grid grid-cols-2 gap-4 mx-4 top-2">
+      <div
+        v-if="product.priceDiscount"
+        class="text-white text-xs w-12 p-1 rounded bg-green-600"
+      >
+        %
+      </div>
+      <div
+        v-if="product.stock == 0"
+        class="text-white text-xs top-2 left-2 p-1 rounded bg-red-600"
+      >
+        out of stock
+      </div>
+    </div>
     <div class="text-left my-4 text-gray-500">
       {{ product.title }}
     </div>
